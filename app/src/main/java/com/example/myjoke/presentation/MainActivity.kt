6cd3de58b4a8960ab8fa38e826c15de0b5e2029a
blastka <svetuk505@mail.ru>
2023-08-28 14:40:56 +0300

    encapsulated getString, getIconId of JokeUi

--- app/src/main/java/com/example/myjoke/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myjoke/presentation/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.myjoke.presentation
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myjoke.JokeApp
 import com.example.myjoke.R
-import com.example.myjoke.data.JokeUi
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,29 +22,37 @@ class MainActivity : AppCompatActivity() {
         val jokeText = findViewById<TextView>(R.id.jokeText)
         val progress = findViewById<ProgressBar>(R.id.progressBar)
         val image = findViewById<ImageButton>(R.id.imageButton)
+        val checkBox = findViewById<CheckBox>(R.id.checkBox)
 
         val callback =
-            object : TextCallback {
+            object : ViewModelCallback {
                 override fun setText(text: String) {
                     progress.isIndeterminate = false
                     jokeText.text = text
                     buttonJoke.isEnabled = true
                 }
 
-                override fun setIcon(id: JokeUi) {
-                    image.setImageResource(id.getIconId())
+                override fun setIcon(id: Int) {
+                    image.setImageResource(id)
                 }
             }
 
+        viewModel.init(callback)
+
         buttonJoke.setOnClickListener {
             progress.isIndeterminate = true
-            viewModel.joke(callback)
+            viewModel.joke()
             buttonJoke.isEnabled = false
+        }
+
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.chooseFavorites(isChecked)
         }
     }
 }
 
-interface TextCallback {
+interface ViewModelCallback {
     fun setText(text: String)
-    fun setIcon(id: JokeUi)
+    fun setIcon(id: Int)
 }
+
